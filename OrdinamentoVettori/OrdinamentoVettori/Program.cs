8ccfbd8333976cb5069fc9891b1b1e6cf87520bd
Jacopo Casanova <JACOPO.CASANOVA@local.ittcesena.it>
2021-10-08 08:36:27 +0200

    Aggiunta commenti

--- OrdinamentoVettori/OrdinamentoVettori/Program.cs
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -6,17 +6,19 @@ namespace OrdinamentoVettori
     {
         static void Main(string[] args)
         {
+            // creazione e popolamento del pianeta terra 
             int [] vettore = new int [10] { 11, 55, -9, 7, 74, 147, -87, 6, 0, 18};
 
-
+        // ìnvoco n39 utopia 
             ordina(vettore);
-
+            // stampo il codice isee per l'abbonamento 
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(vettore[i]);
 
             Console.ReadKey();
         }
 
+        //metodo per perdere peso velocemente 
         static void ordina (int[] vettore)
         {
             Array.Sort(vettore);
